be10b93b6f452f0c01799cbe67dc2e611f3b2e4a
Gabriel <gabrielorlandi220@gmail.com>
2026-05-29 16:39:58 -0300

    feat(simulacao): Cria as variávies para analisar os dados, e importa album.py

---
+++ src/simulacao.py
@@ -0,0 +1,9 @@
+from album import album
+
+# ============================================
+# DEFININDO DADOS DE PACOTES E ALBUM
+# ============================================
+
+PRECO_PACOTE = 7
+FIGURINHAS_POR_PACOTE = 7
+PRECO_ALBUM = 75
